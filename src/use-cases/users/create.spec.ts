6c00cd0ca711e46ff9aabb68bfabd15392b04d51
Leonardo Furtado <leonardomf1996@gmail.com>
2023-07-27 20:37:04 -0300

    test: should hash user password upon registration

--- src/use-cases/users/create.spec.ts
+++ src/use-cases/users/create.spec.ts
@@ -1,6 +1,7 @@
 import { expect, describe, it, beforeEach } from 'vitest'
 import { CreateUseCase } from './create'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { compare } from 'bcryptjs';
 
 let usersRepository: InMemoryUsersRepository;
 let sut: CreateUseCase;
@@ -22,5 +23,20 @@ describe('Create user use case', () => {
       });
 
       expect(user.id).toEqual(expect.any(String));
+   });
+
+   it('should hash user password upon registration', async () => {
+      const { user } = await sut.execute({
+         name: 'John Doe',
+         mail: 'john@mail.com',
+         fullAddress: 'full address, 30',
+         phone: '14999999999',
+         postalCode: '88888888',
+         password: 'abc123',
+      });
+
+      const isPasswordCorrectlyHashed = await compare('abc123', user.passwordHashed);
+
+      expect(isPasswordCorrectlyHashed).toBe(true);
    });
 })
