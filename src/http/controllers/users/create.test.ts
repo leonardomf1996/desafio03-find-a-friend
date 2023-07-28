fba1a1e11e7ed24f77e90028e256ac4a930ef25a
Leonardo Furtado <leonardomf1996@gmail.com>
2023-07-27 21:27:06 -0300

    test: should be able to create a user

---
+++ src/http/controllers/users/create.test.ts
@@ -0,0 +1,28 @@
+import request from 'supertest';
+import { describe, it, expect, beforeAll, afterAll } from 'vitest';
+import { app } from '@/app';
+
+describe('Register (e2e)', () => {
+   beforeAll(async () => {
+      await app.ready();
+   })
+
+   afterAll(async () => {
+      await app.close();
+   })
+
+   it('should be able to create', async () => {
+      const response = await request(app.server)
+         .post('/users')
+         .send({
+            name: 'John Doe',
+            mail: 'johndoe@example.com',
+            password: 'abc123456',
+            fullAddress: 'full address, 30',
+            phone: '14999999999',
+            postalCode: '8888888'
+         })
+
+      expect(response.statusCode).toEqual(201)
+   })
+});
